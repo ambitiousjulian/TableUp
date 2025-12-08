576df871fecbd8cac8f36cb4435ef7c508c375ae
Julian <julian.r.cajus>
2025-12-07 21:42:30 -0500

    Initial Commit

---
+++ TableUp/TableUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableUpApp.swift
+//  TableUp
+//
+//  Created by Julian  on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TableUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
